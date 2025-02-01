f8cdcc6457a1c04f4672cd37c401cdb972f6f12d
Jasmine Smith <61719007+JasmineSmithWGU@users.noreply.github.com>
2025-02-01 15:29:06 -0600

    Update main.py

--- main.py
+++ main.py
@@ -88,7 +88,6 @@ def go(config: DictConfig):
             pass
 
         if "train_random_forest" in active_steps:
-
             # NOTE: we need to serialize the random forest configuration into JSON
             rf_config = os.path.abspath("rf_config.json")
             with open(rf_config, "w+") as fp:
